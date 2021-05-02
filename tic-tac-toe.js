c02e749f990b006811e176f199b0d95d0852933e
Shaan Legros <shaan.legros@gmail.com>
2021-05-01 20:46:03 -0400

    finished design

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,7 @@
-console.log("hello")
+const gameBoard = document.getElementById("gameBoard")
+
+for (let i = 0; i<9; i++) {
+    let boardBox = document.createElement("div")
+    boardBox.className = "boxes"
+    gameBoard.appendChild(boardBox)
+}
